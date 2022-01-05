4e24cacac112fd14fd15a9a29060351c81c3e546
Flora <laszlo.t.flora@gmail.com>
2022-01-05 16:23:07 +0100

    button comp

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Button from './component/button';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Button text = 'Click me' />
+    </div>
+  );
+}
+
+export default App;
